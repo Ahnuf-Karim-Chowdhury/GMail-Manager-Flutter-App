569204cf1dafab18b00f5aca859ad8ea0695d3c8
ahnuf <ahnufkarimchowdhury@gmail.com>
2025-03-11 21:27:37 +0600

    home.dart added

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:gmail_manager/pages/home.dart';
+import 'package:gmail_manager/pages/login.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
@@ -7,13 +9,18 @@ class AuthPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(), 
-        builder: (context,snapshot){
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
           //user looged in
-
+          if (snapshot.hasData) {
+            return HomePage();
+          }
           // user not logged in
-          
-        }),
+          else {
+            return LoginPage();
+          }
+        },
+      ),
     );
   }
 }
